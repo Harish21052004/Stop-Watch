01d91b57d7d09639eb0b41452eb0be5276f7574c
Harish21052004 <126686602+Harish21052004@users.noreply.github.com>
2023-06-11 13:50:26 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,79 @@
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      seconds: 0,
+      minutes: 0,
+      hours: 0,
+      isRunning: false
+    };
+  }
+
+  startTimer = () => {
+    this.setState({
+      isRunning: true
+    });
+    this.timer = setInterval(() => {
+      let { seconds, minutes, hours } = this.state;
+      seconds++;
+      if (seconds >= 60) {
+        seconds = 0;
+        minutes++;
+      }
+      if (minutes >= 60) {
+        minutes = 0;
+        hours++;
+      }
+      this.setState({
+        seconds,
+        minutes,
+        hours
+      });
+    }, 1000);
+  }
+
+  stopTimer = () => {
+    clearInterval(this.timer);
+    this.setState({
+      isRunning: false
+    });
+  }
+
+  resetTimer = () => {
+    clearInterval(this.timer);
+    this.setState({
+      seconds: 0,
+      minutes: 0,
+      hours: 0,
+      isRunning: false
+    });
+  }
+
+  formatTime = (time) => {
+    return time < 10 ? `0${time}` : time;
+  }
+
+  render() {
+    const { seconds, minutes, hours, isRunning } = this.state;
+    return (
+      <div className="Container">
+        <div className='wrapper'>
+          <h2>STOP WATCH</h2>
+          <div className="Time">
+            {this.formatTime(hours)}:{this.formatTime(minutes)}:{this.formatTime(seconds)}
+          </div>
+          <div className="buttons">
+            <button onClick={this.stopTimer} disabled={!isRunning}>Stop</button>
+            <button onClick={this.startTimer} disabled={isRunning}>Start</button>
+            <button onClick={this.resetTimer}>Reset</button>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
